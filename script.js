8ff700bcfd1075a8df12153dfded15e95195b266
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-01-07 16:32:26 -0600

    created the genPass function and accompanying prompts and variables

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
-var num = (1,2,3,4,5,6,7,8,9,0);
-var lowerCase= ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
-var upperCase=("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-var specChar=();
+var howLong = prompt("How long do you want you password to be? (8-128 characters)");
+    console.log(howLong);
+
+
+
+var wantUpper = confirm("Do you want to use uppercase letters?");
+    console.log(wantUpper);
+
+
+var wantLower = confirm("Do you want to use lowercase letters?");
+    console.log(wantLower);
+
+var wantNum = confirm("Do you want to use numbers?");
+    console.log(wantNum);
+
+var wantSpec = confirm("Do you want to use special characters (;/,#%^& etc..)?");
+    console.log(wantSpec);
 
 function genPass(){
+    var all = '';
+    var num = "0123456789";
+    var lowerCase= "abcdefghijklmnopqrstuvqxyz";
+    var upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var specChar= '!#$%"&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
 
-}
 
-function copyClip(){
+    if (wantUpper === true){
+        all +=  upperCase;
+    }
+
+    if (wantLower === true){
+        all += lowerCase;
+    }
+    if (wantNum === true){
+        all += num;
+    }
     
+    if (wantSpec === true){
+        all += specChar;
+    }
+   
+    var password = ''
+    
+        for(var i = 0; i < parseInt(howLong); i++){
+        
+            var randomPick = all[Math.floor(Math.random() * all.length)]
+            
+            password += randomPick;
+        }
+        console.log('This is your password', password);
+             var blankInput = document.querySelector("#passBox");
+             blankInput.value = password;
+    
+  
+
 }
+genPass()
